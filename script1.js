b76beb352382f77e61e1dad9f3036f1c5ef57299
abhishek2446 <abhisheks200426@gmail.com>
2025-08-07 22:43:10 +0530

    Update script1.js

---
+++ script1.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('#multiple-text',  {
+    strings: ['Web Developer','Graphic Designer','Video Editor'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+ });
+ const typeds = new Typed('#multiple-text1',  {
+    strings: ['Web Developer','Graphic Designer','Video Editor'],
+    typeSpeed: 150,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+ });
